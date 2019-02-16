91f9de8a9b8f10a20397676236d6bb85d2c4d7d5
Amanda <akharrett@gmail.com>
2019-02-16 14:16:03 -0800

    Early version of the GUI Non-functioning buttons Placeholder text implemented. Would be better as labels. Formatted code, added comments, put in button placeholders

--- Calibration_GUI/main.py
+++ Calibration_GUI/main.py
@@ -26,60 +26,68 @@ class MainWindow(QMainWindow):
         layout = QVBoxLayout()  # This is where we put all our widgets, which will go in the window
 
         # Start adding widgets to our layout!
+        # Unit choice
         layout.addWidget(QRadioButton('Metric Units'))
         layout.addWidget(QRadioButton('Imperial Units'))
-
-        mass = QLineEdit()
-        mass.setPlaceholderText('Mass')
-        layout.addWidget(mass)
-
+        # Axle width
         axle = QLineEdit()
         axle.setPlaceholderText('Axle Width')
         layout.addWidget(axle)
-
+        # Wheel radius
         wheel_rad = QLineEdit()
         wheel_rad.setPlaceholderText('Wheel Radius')
         layout.addWidget(wheel_rad)
-
+        # Height, axle-to-top
         height = QLineEdit()
         height.setPlaceholderText('Height (Axle-to-top)')
         layout.addWidget(height)
-
+        # Mass of entire robot
+        mass = QLineEdit()
+        mass.setPlaceholderText('Mass (entire robot, assembled)')
+        layout.addWidget(mass)
+        # Wheel Mass
         wheel_mass = QLineEdit()
         wheel_mass.setPlaceholderText('Wheel Mass (each)')
         layout.addWidget(wheel_mass)
-
+        # Load Mass
         load_mass = QLineEdit()
         load_mass.setPlaceholderText('Mass of Load (only)')
         layout.addWidget(load_mass)
-
+        # Body Mass (no load, no wheels)
         body_mass = QLineEdit()
         body_mass.setPlaceholderText('Body Mass (no load, no wheels)')
         layout.addWidget(body_mass)
-
+        # Confirm and Cancel buttons
         layout.addWidget(QPushButton('Confirm'))
         layout.addWidget(QPushButton('Cancel'))
         layout.addWidget(QSlider())
-
+        # Initialize all of the above as a single Widget
         widget = QWidget()
         widget.setLayout(layout)  # this executes the layout we defined earlier
-
+        # Create widget, set location and size of window that appears
         self.setCentralWidget(widget)
-        self.setGeometry(40, 80, 500, 380) # Set window offset and size
+        self.setGeometry(40, 80, 500, 380) # offset, offset, size, size
 
-
-
+# TO DO: implement button function
 # class CancelButton(Qbutton):
 #
-#     def keyPressEvent(selfself, e):
+#     def keyPressEvent(self, e):
 #         # when you click 'Cancel'
 #         super(CancelButton, self).keyPressEvent(e)
 
-app = QApplication(sys.argv) #every app must have at least one instance of this
+# class ConfirmButton(Qbutton):
+# # This button should interpret all of the numbers and verify that all fields were filled in
+# # If all fields not filled in, then the process cancels
+# # If all fields are filled in with acceptable data types, pass the numbers and the
+# # choice of units along to a JSON file in the form of a dict
+#     def keyPRessEvent(self, e):
+#     # when you click "Confirm"
+#         super(ConfirmButton, self).keyPressEvent(e)
 
+app = QApplication(sys.argv) #every app must have at least one instance of this
 window = MainWindow() # this is our container!
 
 # Always end with calls to show() and to exec_()
 window.show() # make the window appear
 app.exec_() # tells the program to run until the user closes it
-
+# end program
