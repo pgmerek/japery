f06a2f17730f05919c10e35aaaa39b7b59a848f3
Amanda <akharrett@gmail.com>
2019-02-23 11:59:29 -0800

    Confirm button displays the contents of text entries. Cancel button placeholder implemented

--- Calibration_GUI/main.py
+++ Calibration_GUI/main.py
@@ -24,43 +24,58 @@ class MainWindow(QMainWindow):
         label = QLabel("Some label")
         label.setAlignment(Qt.AlignCenter)
         layout = QVBoxLayout()  # This is where we put all our widgets, which will go in the window
+        # define validator for number inputs
+        dbl_valid = QDoubleValidator(0.0000, 99999.9999, 4)
 
         # Start adding widgets to our layout!
         # Unit choice
         layout.addWidget(QRadioButton('Metric Units'))
         layout.addWidget(QRadioButton('Imperial Units'))
         # Axle width
-        axle = QLineEdit()
-        axle.setPlaceholderText('Axle Width')
-        layout.addWidget(axle)
+        self.axle = QLineEdit()
+        self.axle.setPlaceholderText('Axle Width')
+        self.axle.setValidator(dbl_valid)
+        layout.addWidget(self.axle)
         # Wheel radius
-        wheel_rad = QLineEdit()
-        wheel_rad.setPlaceholderText('Wheel Radius')
-        layout.addWidget(wheel_rad)
+        self.wheel_rad = QLineEdit()
+        self.wheel_rad.setPlaceholderText('Wheel Radius')
+        self.wheel_rad.setValidator(dbl_valid)
+        layout.addWidget(self.wheel_rad)
         # Height, axle-to-top
-        height = QLineEdit()
-        height.setPlaceholderText('Height (Axle-to-top)')
-        layout.addWidget(height)
+        self.height = QLineEdit()
+        self.height.setPlaceholderText('Height (Axle-to-top)')
+        self.height.setValidator(dbl_valid)
+        layout.addWidget(self.height)
         # Mass of entire robot
-        mass = QLineEdit()
-        mass.setPlaceholderText('Mass (entire robot, assembled)')
-        layout.addWidget(mass)
+        self.mass = QLineEdit()
+        self.mass.setPlaceholderText('Mass (entire robot, assembled)')
+        self.mass.setValidator(dbl_valid)
+        layout.addWidget(self.mass)
         # Wheel Mass
-        wheel_mass = QLineEdit()
-        wheel_mass.setPlaceholderText('Wheel Mass (each)')
-        layout.addWidget(wheel_mass)
+        self.wheel_mass = QLineEdit()
+        self.wheel_mass.setPlaceholderText('Wheel Mass (each)')
+        self.wheel_mass.setValidator(dbl_valid)
+        layout.addWidget(self.wheel_mass)
         # Load Mass
-        load_mass = QLineEdit()
-        load_mass.setPlaceholderText('Mass of Load (only)')
-        layout.addWidget(load_mass)
+        self.load_mass = QLineEdit()
+        self.load_mass.setPlaceholderText('Mass of Load (only)')
+        self.load_mass.setValidator(dbl_valid)
+        layout.addWidget(self.load_mass)
         # Body Mass (no load, no wheels)
-        body_mass = QLineEdit()
-        body_mass.setPlaceholderText('Body Mass (no load, no wheels)')
-        layout.addWidget(body_mass)
-        # Confirm and Cancel buttons
-        layout.addWidget(QPushButton('Confirm'))
-        layout.addWidget(QPushButton('Cancel'))
-        layout.addWidget(QSlider())
+        self.body_mass = QLineEdit()
+        self.body_mass.setPlaceholderText('Body Mass (no load, no wheels)')
+        self.body_mass.setValidator(dbl_valid)
+        layout.addWidget(self.body_mass)
+        # Confirm Button
+        confirm_button = QPushButton('Confirm')
+        confirm_button.pressed.connect(self.ConfirmButtonFunc)
+        layout.addWidget(confirm_button)
+        # Cancel Button
+        cancel_button = QPushButton('Cancel')
+        cancel_button.pressed.connect(self.CancelButtonFunc)
+        layout.addWidget(cancel_button)
+
+        # layout.addWidget(QSlider())
         # Initialize all of the above as a single Widget
         widget = QWidget()
         widget.setLayout(layout)  # this executes the layout we defined earlier
@@ -68,21 +83,28 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
         self.setGeometry(40, 80, 500, 380) # offset, offset, size, size
 
-# TO DO: implement button function
-# class CancelButton(Qbutton):
-#
-#     def keyPressEvent(self, e):
-#         # when you click 'Cancel'
-#         super(CancelButton, self).keyPressEvent(e)
+    # TO DO: implement button function
+    # class CancelButton(Qbutton):
+    #
+    #     def keyPressEvent(self, e):
+    #         # when you click 'Cancel'
+    #         super(CancelButton, self).keyPressEvent(e)
 
-# class ConfirmButton(Qbutton):
-# # This button should interpret all of the numbers and verify that all fields were filled in
-# # If all fields not filled in, then the process cancels
-# # If all fields are filled in with acceptable data types, pass the numbers and the
-# # choice of units along to a JSON file in the form of a dict
-#     def keyPRessEvent(self, e):
-#     # when you click "Confirm"
-#         super(ConfirmButton, self).keyPressEvent(e)
+    def ConfirmButtonFunc(self):
+        print("I'm a button!")
+        print("Axle width: " + self.axle.text())
+        print("Wheel radius: " + self.wheel_rad.text())
+        print("Height: " + self.height.text())
+        print("Mass of entire robot: " + self.mass.text())
+        print("Wheel Mass (each); " + self.wheel_mass.text())
+        print("Load mass: " + self.load_mass.text())
+        print("Body mass: " + self.body_mass.text())
+
+    def CancelButtonFunc(self):
+        print("You're CANCELED")
+
+
+
 
 app = QApplication(sys.argv) #every app must have at least one instance of this
 window = MainWindow() # this is our container!
