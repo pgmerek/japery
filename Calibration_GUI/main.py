35c0dc76c1a74a3fa1ff883072ced31e0d45bc7f
akharrett <35310096+akharrett@users.noreply.github.com>
2019-02-25 17:25:59 -0800

    Update main.py
    Added labels and rearranged the vertical and horizontal box components. Appearance upgrade!

--- Calibration_GUI/main.py
+++ Calibration_GUI/main.py
@@ -20,70 +20,103 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Robot Configuration")
 
-        label = QLabel("Some label")
-        label.setAlignment(Qt.AlignCenter)
-        layout = QVBoxLayout()  # This is where we put all our widgets, which will go in the window
+        main_vertical_layout = QVBoxLayout()  # This is where we put all our widgets, which will go in the window
+        main_vertical_layout.setSpacing(1) # makes it look nice
+        inside_left_vert_layout = QVBoxLayout()
+        inside_right_vert_layout = QVBoxLayout()
+        horizontal_layout = QHBoxLayout()
+        horizontal_layout.setSpacing(10)
+        
         # define validator for number inputs
         dbl_valid = QDoubleValidator(0.0000, 99999.9999, 4)
 
         # Start adding widgets to our layout!
         # Unit choice
-        layout.addWidget(QRadioButton('Metric Units'))
-        layout.addWidget(QRadioButton('Imperial Units'))
+        inside_left_vert_layout.addWidget(QRadioButton('Metric Units'))
+        inside_right_vert_layout.addWidget(QRadioButton('Imperial Units'))
+
         # Axle width
         self.axle = QLineEdit()
         self.axle.setPlaceholderText('Axle Width')
         self.axle.setValidator(dbl_valid)
-        layout.addWidget(self.axle)
+        inside_right_vert_layout.addWidget(self.axle)
+        axle_label = QLabel('Axle Width')
+        inside_left_vert_layout.addWidget(axle_label)
+
         # Wheel radius
         self.wheel_rad = QLineEdit()
         self.wheel_rad.setPlaceholderText('Wheel Radius')
         self.wheel_rad.setValidator(dbl_valid)
-        layout.addWidget(self.wheel_rad)
+        inside_right_vert_layout.addWidget(self.wheel_rad)
+        wheel_rad_label = QLabel('Wheel Radius')
+        inside_left_vert_layout.addWidget(wheel_rad_label)
+
         # Height, axle-to-top
         self.height = QLineEdit()
         self.height.setPlaceholderText('Height (Axle-to-top)')
         self.height.setValidator(dbl_valid)
-        layout.addWidget(self.height)
+        inside_right_vert_layout.addWidget(self.height)
+        height_label = QLabel('Height (Axle-to-top)')
+        inside_left_vert_layout.addWidget(height_label)
+
+
         # Mass of entire robot
         self.mass = QLineEdit()
         self.mass.setPlaceholderText('Mass (entire robot, assembled)')
         self.mass.setValidator(dbl_valid)
-        layout.addWidget(self.mass)
+        inside_right_vert_layout.addWidget(self.mass)
+        mass_label = QLabel('Mass (entire robot, assembled)')
+        inside_left_vert_layout.addWidget(mass_label)
+
         # Wheel Mass
         self.wheel_mass = QLineEdit()
         self.wheel_mass.setPlaceholderText('Wheel Mass (each)')
         self.wheel_mass.setValidator(dbl_valid)
-        layout.addWidget(self.wheel_mass)
+        inside_right_vert_layout.addWidget(self.wheel_mass)
+        wheel_mass_label = QLabel('Wheel Mass (each)')
+        inside_left_vert_layout.addWidget(wheel_mass_label)
+
         # Load Mass
         self.load_mass = QLineEdit()
         self.load_mass.setPlaceholderText('Mass of Load (only)')
         self.load_mass.setValidator(dbl_valid)
-        layout.addWidget(self.load_mass)
+        inside_right_vert_layout.addWidget(self.load_mass)
+        load_mass_label = QLabel('Mass of Load (only)')
+        inside_left_vert_layout.addWidget(load_mass_label)
+
         # Body Mass (no load, no wheels)
         self.body_mass = QLineEdit()
         self.body_mass.setPlaceholderText('Body Mass (no load, no wheels)')
         self.body_mass.setValidator(dbl_valid)
-        layout.addWidget(self.body_mass)
+        inside_right_vert_layout.addWidget(self.body_mass)
+        body_mass_label = QLabel('Body Mass (no load, no wheels)')
+        inside_left_vert_layout.addWidget(body_mass_label)
+
+        # now add those to the main layout here
+        horizontal_layout.addLayout(inside_left_vert_layout) # add the left box contents
+        horizontal_layout.addLayout(inside_right_vert_layout) # add the right box contents
+        main_vertical_layout.addLayout(horizontal_layout)
         # Confirm Button
         confirm_button = QPushButton('Confirm')
         confirm_button.pressed.connect(self.ConfirmButtonFunc)
-        layout.addWidget(confirm_button)
+        main_vertical_layout.addWidget(confirm_button)
         # Cancel Button
         cancel_button = QPushButton('Cancel')
         cancel_button.pressed.connect(self.CancelButtonFunc)
-        layout.addWidget(cancel_button)
+        main_vertical_layout.addWidget(cancel_button)
 
         # layout.addWidget(QSlider())
         # Initialize all of the above as a single Widget
         widget = QWidget()
-        widget.setLayout(layout)  # this executes the layout we defined earlier
+
+
+        widget.setLayout(main_vertical_layout)  # make the vertical layout the primary container
         # Create widget, set location and size of window that appears
         self.setCentralWidget(widget)
-        self.setGeometry(40, 80, 500, 380) # offset, offset, size, size
-        
+        self.setGeometry(40, 80, 600, 380) # offset, offset, width size, height size
+
     def ConfirmButtonFunc(self):
-        print("Saved data to output.json.")
+        print("Saved data to output.json")
         # print("Axle width: " + self.axle.text())
         # print("Wheel radius: " + self.wheel_rad.text())
         # print("Height: " + self.height.text())
@@ -91,6 +124,9 @@ class MainWindow(QMainWindow):
         # print("Wheel Mass (each); " + self.wheel_mass.text())
         # print("Load mass: " + self.load_mass.text())
         # print("Body mass: " + self.body_mass.text())
+
+        # DATA QUALITY CHECK CODE GOES HERE. Are all values filled in? Units? 
+        
         dict = {'axle width': self.axle.text(), 'wheel radius': self.wheel_rad.text(), 
             'height': self.height.text(), 'Robot Mass': self.mass.text(), 
             'Wheel Mass': self.wheel_mass.text(), 'Load Mass': self.load_mass.text(), 
