d9333e9f32bbb83b918b2b4badb855d1e1ae227a
akharrett <35310096+akharrett@users.noreply.github.com>
2019-02-27 11:09:51 -0800

    Update main.py
    Removed Imperial and Metric radio buttons, added drop-down menus for unit selection. Dropdowns don't do anything yet.

--- Calibration_GUI/main.py
+++ Calibration_GUI/main.py
@@ -1,16 +1,29 @@
 '''This is the GUI for the Self-Balancing Robot Settings Page.
-This code is intended to collect measurements and mass of the parts of a two-wheeled
+This code is intended to collect measurements and Weight of the parts of a two-wheeled
 inverted pendulum robot. The user can select Imperial or Metric unit types, and then the
 data is collected and output to a JSON file.
 Amanda Voegtlin, 2019
 Portland State University'''
 
 from PyQt5.QtWidgets import * # QApplication, QWidget, QLineEdit, QLabel, QCheckBox, QRadioButton, QPushButton, QVBoxLayout
-        # QVBoxLayout stacks widgets vertically
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 import json
 import sys
+
+class Weight_Menu(QComboBox): # Making this a class allows it to be duplicated
+    def __init__(self, parent=None):
+        QComboBox.__init__(self, parent=parent)
+        self.addItem("grams")
+        self.addItem("kilograms")
+        self.addItem("lbs")
+        self.addItem("ounces")
+
+class Size_Menu(QComboBox): # Making this a class allows it to be duplicated
+    def __init__(self, parent=None):
+        QComboBox.__init__(self, parent=parent)
+        self.addItem("inches")
+        self.addItem("centimeters")
 
 # This subclass will set our main window up
 class MainWindow(QMainWindow):
@@ -23,6 +36,7 @@ class MainWindow(QMainWindow):
         main_vertical_layout = QVBoxLayout()  # This is where we put all our widgets, which will go in the window
         main_vertical_layout.setSpacing(1) # makes it look nice
         inside_left_vert_layout = QVBoxLayout()
+        inside_middle_vert_layout = QVBoxLayout()
         inside_right_vert_layout = QVBoxLayout()
         horizontal_layout = QHBoxLayout()
         horizontal_layout.setSpacing(10)
@@ -30,70 +44,80 @@ class MainWindow(QMainWindow):
         # define validator for number inputs
         dbl_valid = QDoubleValidator(0.0000, 99999.9999, 4)
 
-        # Start adding widgets to our layout!
-        # Unit choice
-        inside_left_vert_layout.addWidget(QRadioButton('Metric Units'))
-        inside_right_vert_layout.addWidget(QRadioButton('Imperial Units'))
-
         # Axle width
         self.axle = QLineEdit()
         self.axle.setPlaceholderText('Axle Width')
         self.axle.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.axle)
+        inside_middle_vert_layout.addWidget(self.axle)
         axle_label = QLabel('Axle Width')
         inside_left_vert_layout.addWidget(axle_label)
+        axle_dropdown = Size_Menu()
+        inside_right_vert_layout.addWidget(axle_dropdown)
 
         # Wheel radius
         self.wheel_rad = QLineEdit()
         self.wheel_rad.setPlaceholderText('Wheel Radius')
         self.wheel_rad.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.wheel_rad)
+        inside_middle_vert_layout.addWidget(self.wheel_rad)
         wheel_rad_label = QLabel('Wheel Radius')
         inside_left_vert_layout.addWidget(wheel_rad_label)
+        wheel_rad_dropdown = Size_Menu()
+        inside_right_vert_layout.addWidget(wheel_rad_dropdown)
 
         # Height, axle-to-top
         self.height = QLineEdit()
         self.height.setPlaceholderText('Height (Axle-to-top)')
         self.height.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.height)
+        inside_middle_vert_layout.addWidget(self.height)
         height_label = QLabel('Height (Axle-to-top)')
         inside_left_vert_layout.addWidget(height_label)
+        height_dropdown = Size_Menu()
+        inside_right_vert_layout.addWidget(height_dropdown)
 
-        # Mass of entire robot
-        self.mass = QLineEdit()
-        self.mass.setPlaceholderText('Mass (entire robot, assembled)')
-        self.mass.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.mass)
-        mass_label = QLabel('Mass (entire robot, assembled)')
+        # Weight of entire robot
+        self.Weight = QLineEdit()
+        self.Weight.setPlaceholderText('Weight (entire robot, assembled)')
+        self.Weight.setValidator(dbl_valid)
+        inside_middle_vert_layout.addWidget(self.Weight)
+        mass_label = QLabel('Weight (entire robot, assembled)')
         inside_left_vert_layout.addWidget(mass_label)
+        mass_dropdown = Weight_Menu()
+        inside_right_vert_layout.addWidget(mass_dropdown)
 
-        # Wheel Mass
+        # Wheel Weight
         self.wheel_mass = QLineEdit()
-        self.wheel_mass.setPlaceholderText('Wheel Mass (each)')
+        self.wheel_mass.setPlaceholderText('Wheel Weight (each)')
         self.wheel_mass.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.wheel_mass)
-        wheel_mass_label = QLabel('Wheel Mass (each)')
+        inside_middle_vert_layout.addWidget(self.wheel_mass)
+        wheel_mass_label = QLabel('Wheel Weight (each)')
         inside_left_vert_layout.addWidget(wheel_mass_label)
+        wheel_mass_dropdown = Weight_Menu()
+        inside_right_vert_layout.addWidget(wheel_mass_dropdown)
 
-        # Load Mass
+        # Load Weight
         self.load_mass = QLineEdit()
-        self.load_mass.setPlaceholderText('Mass of Load (only)')
+        self.load_mass.setPlaceholderText('Weight of Load (only)')
         self.load_mass.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.load_mass)
-        load_mass_label = QLabel('Mass of Load (only)')
+        inside_middle_vert_layout.addWidget(self.load_mass)
+        load_mass_label = QLabel('Weight of Load (only)')
         inside_left_vert_layout.addWidget(load_mass_label)
+        load_mass_dropdown = Weight_Menu()
+        inside_right_vert_layout.addWidget(load_mass_dropdown)
 
-        # Body Mass (no load, no wheels)
-        self.body_mass = QLineEdit()
-        self.body_mass.setPlaceholderText('Body Mass (no load, no wheels)')
-        self.body_mass.setValidator(dbl_valid)
-        inside_right_vert_layout.addWidget(self.body_mass)
-        body_mass_label = QLabel('Body Mass (no load, no wheels)')
+        # Body Weight (no load, no wheels)
+        body_mass_label = QLabel('Body Weight (no load, no wheels)')
         inside_left_vert_layout.addWidget(body_mass_label)
+        self.body_mass = QLineEdit()
+        self.body_mass.setPlaceholderText('Body Weight (no load, no wheels)')
+        self.body_mass.setValidator(dbl_valid)
+        inside_middle_vert_layout.addWidget(self.body_mass)
+        body_mass_dropdown = Weight_Menu()
+        inside_right_vert_layout.addWidget(body_mass_dropdown)
 
         # now add those to the main layout here
         horizontal_layout.addLayout(inside_left_vert_layout) # add the left box contents
-        horizontal_layout.addLayout(inside_right_vert_layout) # add the right box contents
+        horizontal_layout.addLayout(inside_middle_vert_layout) # add the middle box contents
+        horizontal_layout.addLayout(inside_right_vert_layout) # add the box with the dropdowns
         main_vertical_layout.addLayout(horizontal_layout)
         # Confirm Button
         confirm_button = QPushButton('Confirm')
@@ -107,6 +131,8 @@ class MainWindow(QMainWindow):
         # layout.addWidget(QSlider())
         # Initialize all of the above as a single Widget
         widget = QWidget()
+
+
         widget.setLayout(main_vertical_layout)  # make the vertical layout the primary container
         # Create widget, set location and size of window that appears
         self.setCentralWidget(widget)
@@ -114,18 +140,28 @@ class MainWindow(QMainWindow):
 
     def ConfirmButtonFunc(self):
         print("Saved data to output.json")
+        # print("Axle width: " + self.axle.text())
+        # print("Wheel radius: " + self.wheel_rad.text())
+        # print("Height: " + self.height.text())
+        # print("Weight of entire robot: " + self.Weight.text())
+        # print("Wheel Weight (each); " + self.wheel_mass.text())
+        # print("Load Weight: " + self.load_mass.text())
+        # print("Body Weight: " + self.body_mass.text())
+
         # DATA QUALITY CHECK CODE GOES HERE. Are all values filled in? Units? 
+        
         dict = {'axle width': self.axle.text(), 'wheel radius': self.wheel_rad.text(), 
-            'height': self.height.text(), 'Robot Mass': self.mass.text(), 
-            'Wheel Mass': self.wheel_mass.text(), 'Load Mass': self.load_mass.text(), 
-            'Body Mass': self.body_mass.text()}
+            'height': self.height.text(), 'Robot Weight': self.Weight.text(), 
+            'Wheel Weight': self.wheel_mass.text(), 'Load Weight': self.load_mass.text(), 
+            'Body Weight': self.body_mass.text()}
         file = open('output.json', 'w+') #creates output.json if it doesn't exist, opens and truncates if it does 
         json.dump(dict, file)
         
+
     def CancelButtonFunc(self):
-        # This should eventually do something, like discard changes or close the GUI
         print("You're CANCELED")
-# ---- Main -----
+
+
 app = QApplication(sys.argv) #every app must have at least one instance of this
 window = MainWindow() # this is our container!
 
